f205c9b17c427f97d8e8e09656e84737d7353eb8
root <root@ip-172-31-2-216.us-east-2.compute.internal>
2020-09-23 08:55:13 +0000

    code to pass objects and keys

---
+++ assignemt.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#task3 to pass the nested objects and key and get the same output 
+
+def safeget(dct, mykey):
+    keys=mykey.split('/')
+    for key in keys:
+        try:
+            dct = dct[key]
+        except keyerror:
+            return  None
+        return dct
+    dct = {'x':{'y':{'z'}}}
+    print(safeget(dct,'x/y'))
